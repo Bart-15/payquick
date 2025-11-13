c873f7223594b96cf34a838cca54f577530dfdf3
Bart-15 <brat.tabusao@gmail.com>
2025-11-13 19:19:47 +0800

    chore: 💬 configure ESLint and fix unsort files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import prettierPlugin from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
   globalIgnores([
     // Default ignores of eslint-config-next:
     '.next/**',
@@ -13,6 +15,49 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
-]);
 
-export default eslintConfig;
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        React: 'writable',
+        window: 'readonly',
+        document: 'readonly',
+      },
+    },
+
+    plugins: {
+      prettier: prettierPlugin,
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'no-console': ['warn'],
+      'no-unused-vars': ['warn'],
+      'import/named': 'error',
+      'import/extensions': ['error', 'never'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/no-unresolved': 'off',
+      'consistent-return': 'off',
+
+      // Prettier integration
+      'prettier/prettier': [
+        'warn',
+        {
+          printWidth: 80,
+          singleQuote: true,
+          endOfLine: 'auto',
+          jsxSingleQuote: false,
+        },
+      ],
+    },
+  },
+]);
