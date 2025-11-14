c5eb307609cc97dfe2efa5ef36c4a49c196d60a0
Bart-15 <brat.tabusao@gmail.com>
2025-11-14 21:53:00 +0800

    chore: 🚧 initial dashboard page setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Quick Pay - Dashboard',
+  description: 'Quick Pay - Dashboard',
+};
+
+export default function DashboardPage() {
+  return (
+    <div className="">
+      <p>Hello</p>
+    </div>
+  );
+}
