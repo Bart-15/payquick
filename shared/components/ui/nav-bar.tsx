c1345500fe396e9697eb0a2e7ea78ef1d4b8aee0
Bart-15 <brat.tabusao@gmail.com>
2025-11-14 21:50:20 +0800

    feat: ✨ add navbar component with logout functionality

---
+++ shared/components/ui/nav-bar.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import { authStorage } from '@/features/auth/storage/auth.storage';
+import { Button } from '@/shared/components/ui/button';
+import { removeHeaderToken } from '@/shared/lib/axios-instance';
+
+export const NavBar = () => {
+  const router = useRouter();
+
+  const [auth, setAuth] =
+    useState<ReturnType<typeof authStorage.getAuthSession>>(null);
+
+  // init auth session, and fix hydration mismatch
+  useEffect(() => {
+    function init() {
+      const storedAuth = authStorage.getAuthSession();
+      setAuth(storedAuth);
+    }
+
+    init();
+  }, []);
+
+  function handleLogout() {
+    authStorage.removeAuthSession();
+    removeHeaderToken();
+    router.push('/login');
+  }
+  return (
+    <nav className="flex w-full items-center justify-between bg-white px-6 py-4 shadow-md">
+      <div className="text-lg font-semibold">Quick Pay</div>
+      <div className="flex items-center gap-4">
+        <span className="text-gray-700">Hi, {auth?.user?.full_name}</span>
+        <Button
+          onClick={handleLogout}
+          className="rounded-md bg-red-500 px-4 py-2 text-white transition hover:bg-red-600"
+        >
+          Logout
+        </Button>
+      </div>
+    </nav>
+  );
+};
