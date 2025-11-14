2c2c6fb23686fe260b1101bf2c94cf4e80e47367
Bart-15 <brat.tabusao@gmail.com>
2025-11-14 19:12:52 +0800

    chore: 💬 update imports

--- providers/react-query.tsx
+++ providers/react-query.tsx
@@ -3,7 +3,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { queryClient } from '@/config/query-client';
+import { queryClient } from '@/shared/lib/query-client';
 
 export function ReactQueryProvider({
   children,
