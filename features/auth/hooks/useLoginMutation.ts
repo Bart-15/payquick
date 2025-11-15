078740557d17d8e49df589379917e9ca60fc752b
Bart-15 <brat.tabusao@gmail.com>
2025-11-15 11:32:58 +0800

    fix: 🐛 fix login error handling

--- features/auth/hooks/useLoginMutation.ts
+++ features/auth/hooks/useLoginMutation.ts
@@ -30,7 +30,7 @@ export const useLoginMutation = () => {
     },
 
     onError: (error: AxiosError) => {
-      if (error.response?.status === 404) {
+      if (error.response?.status === 404 || error.response?.status === 401) {
         toast.error('Invalid credentials');
       }
     },
