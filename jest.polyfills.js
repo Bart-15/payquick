42552826e16a7911a05dce6aea26ba507b8a5e13
Bart-15 <brat.tabusao@gmail.com>
2026-03-28 21:25:52 +0800

    feat: ✨ setup msw

---
+++ jest.polyfills.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+// jest.polyfills.js
+
+const { TextDecoder, TextEncoder } = require('node:util');
+
+const { ReadableStream, TransformStream } = require('node:stream/web');
+
+const { BroadcastChannel, MessagePort } = require('node:worker_threads');
+
+Object.defineProperties(globalThis, {
+  TextDecoder: { value: TextDecoder },
+  TextEncoder: { value: TextEncoder },
+  ReadableStream: { value: ReadableStream },
+  TransformStream: { value: TransformStream },
+  BroadcastChannel: { value: BroadcastChannel },
+  MessagePort: { value: MessagePort },
+});
+
+const { Blob, File } = require('node:buffer');
+const { fetch, Headers, FormData, Request, Response } = require('undici');
+
+Object.assign(globalThis, {
+  fetch,
+  Headers,
+  FormData,
+  Request,
+  Response,
+  Blob,
+  File,
+});
--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,12 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// Start API mocking before all tests
+beforeAll(() => server.listen());
+// Reset handlers after each test
+afterEach(() => server.resetHandlers());
+// Clean up after tests
+afterAll(() => server.close());
